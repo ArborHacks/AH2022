901d45d0dfc018b1675cd761a471e7d9e0c12bb3
cgalbus <72706750+cgalbus@users.noreply.github.com>
2022-03-06 23:10:48 +0000

    Carter: test cases for rocketPrices and isAlien

--- autograder/contest_22.h
+++ autograder/contest_22.h
@@ -2,10 +2,59 @@
 #include <string>
 #include <list>
 
-//Example RME:
+using namespace std;
 
-// REQUIRES: startDate is between "01/01/2000" and "12/31/9999"
-// EFFECTS:  Returns the next palindromic date given the starting date.
-//           If the date given is a palindrome, it returns that date.
-//           See instruction document for more information on palindromes.
-std::string palindrome(std::string startDate);
+//Contestants: replace the "throw 0;" with your code!
+
+// Question 1
+// REQUIRES: name is formatted and capitalized correctly if it is Jeff Bezos or one of his aliases given in spec
+// EFFECTS: Returns true if name is "Jeff Bezos" or an alias. Outputs "Found Him!" if it is actually "Jeff Bezos". Outputs the name of the alias if it one of the three given. Outputs "Not Jeff" if neither Jeff Bezos nor an alias.
+bool isBezos(string name);
+
+// Question 2
+// REQUIRES: prices contains a single index that has the value 0
+// EFFECTS: returns a new array that replaces the 0 index with newPrice
+vector<int> newRocketPrices(std::vector<int> prices, int newPrice);
+
+// Question 3
+// REQUIRES: Input is formatted with 16 cities, each with a string of the city followed by a char of its building type (h, w, d, r)
+// EFFECTS: Output modeled after spec: <Building Type>: followed by a ranking of the 4 cities that correspond to that building type. The order of building are Headquarters, Warehouse, Data Center, then Research Center
+void outputCities();
+
+// Question 4
+// REQUIRES: temperatures 2D vector is not empty
+// MODIFIES: hasFever and isMinion flags
+// EFFECTS: temperatures is read with a bandpass of 90-110 degrees, and the hasFever and isMinion flags are set according to the average temperature after the bandpass filter. A fever is an average above 100 degrees, and the lowest human temperature is 96 degrees
+
+void isAlien(vector<vector<int>> temperatures, bool &hasFever, bool &isMinion);
+
+
+//Question 5
+// REQUIRES: stocks vector is at least size 1
+// EFFECTS: returns a new vector that is comprised of the longest, continuous, decreasing subsequence of stocks vector
+vector<int> worstStocks(vector<int> stocks);
+
+
+//Question 6
+
+// Package ADT interface
+struct Package{
+  int length;
+  int width;
+  int height;
+}
+
+// REQUIRES: all packages have lenght, width, height > 0
+// MODIFIES: vector<Package> packages
+// EFFECTS: sorts packages in ascedning order (smallest first)
+void sortPackages(std::vector<Package> &packages);
+
+
+
+// Question 7 --> TODO
+
+
+// Question 8
+// REQUIRES: coins is not empty
+// EFFECTS: any two consecutive coins in the vector cannot both be mined. You must return the maximum value that can be mined under the above constraint.
+int maxJeffCoin(vector<int> coins);
--- autograder/driver_22.cpp
+++ autograder/driver_22.cpp
@@ -6,11 +6,6 @@
 
 using namespace std;
 
-/* Example function prototypes */
-  //change names to new question type
-static pair<uint32_t, uint32_t> testHangman();
-static pair<uint32_t, uint32_t> testVigenere();
-
 //Example of how to set up test:
 //1. set up points pair (what gets returned)
 //2. set up the vectors for input (test) and expected output (keys)
@@ -37,17 +32,27 @@ static pair<uint32_t, uint32_t> testVectors() {
 }
 
 
-/* Example function prototypes */
-  //change names to new question type
 static pair<uint32_t, uint32_t> testIsBezos();
+static pair<uint32_t, uint32_t> testRocketPrices();
+static pair<uint32_t, uint32_t> testOutputCities();
+static pair<uint32_t, uint32_t> testIsAlien();
+static pair<uint32_t, uint32_t> testWorstStocks();
+static pair<uint32_t, uint32_t> testSortPackages();
+// static pair<uint32_t, uint32_t> QUESTION 7
+static pair<uint32_t, uint32_t> testMaxJeffCoin();
+
 
 //Example of how to set up test:
 //1. set up points pair (what gets returned)
 //2. set up the vectors for input (test) and expected output (keys)
 //3. try statement that iterates over the vectors and gives 1 point for every passed test case
 //4. return pair of points (points earned, points available)
+
+// TODO: fix try statement
+// QUESTION 1
 static pair<uint32_t, uint32_t> testIsBezos() {
   pair<uint32_t, uint32_t> points;
+  points.first = 0;
 
   vector<string> tests = {"Beff Bezos",
 "Zuckerberg",
@@ -64,7 +69,7 @@ static pair<uint32_t, uint32_t> testIsBezos() {
   
   try {
     for (int i = 0; i < 12; i++){
-      if (vectors(tests[i]) == keys[i]){
+      if (vectors(tests[i]) == keys[i]){ // TODO vectors() calls the contestant function, also we need to check for cout somehow
         points.first++;
       }
     }
@@ -74,4 +79,110 @@ static pair<uint32_t, uint32_t> testIsBezos() {
 
   points.second = 6;
   cout << "Testing IsBezos: " << points.first << "/" << points.second << endl;
+}
+
+// QUESTION 2 
+static pair<uint32_t, uint32_t> testRocketPrices() {
+  pair<uint32_t, uint32_t> points;
+  points.first = 0;
+
+  vector<vector<int>> tests = {
+    {48, 12, 9, 0, 50, 0, 40, 1, 0, 9},
+    {0},
+    {2, 3, 4, 18, 1, 0},
+    {4, 5, 1, 483, 3181, 123, 0, 13, 34},
+    {0, 1, 2, 3}
+  };
+
+  vector<int> prices = {3, 8, 16, 99, 84};
+
+  vector<vector<int>> keys = {
+
+    {48, 12, 9, 0, 50, 0, 40, 1, 3, 9},
+    {8},
+    {2, 3, 4, 18, 1, 16},
+    {4, 5, 1, 483, 3181, 123, 99, 13, 34},
+    {84, 1, 2, 3}
+
+  };
+
+  try {
+    for (int i = 0; i < 5; i++){
+      if (newRocketPrices(tests[i], prices[i]) == keys[i]){
+        points.first++;
+      }
+    }
+  } catch(...) {
+    points.first = 0;
+  }
+
+    points.second = 5;
+
+   cout << "Testing newRocketPrices: " << points.first << "/" << points.second << endl;
+  
+}
+
+// QUESTION 3
+// TODO: buffers & how to check with cout
+
+// QUESTION 4
+static pair<uint32_t, uint32_t> testIsAlien(){
+  pair<uint32_t, uint32_t> points;
+  points.first = 0;
+  points.second = 4;
+
+  vector<vector<vector<int>>> tests = {
+    { // Test 1
+    {0, 0, 0},
+    {0, 95, 0},
+    {0, 96, 0},
+    {0, 97, 0},
+    {0, 0, 0}
+    },
+    { // Test 2
+    {0, 111, 0},
+    {120, 120, 120},
+    {0, 0, 0},
+    {120, 120, 120},
+    {0, 89, 0}
+    },
+    { // Test 3
+    {100, 101, 102},
+    {100, 101, 102},
+    {100, 101, 102},
+    {100, 101, 102},
+    {100, 101, 102}
+    },
+    { // Test 4
+    {89, 90, 91},
+    {92, 93, 94},
+    {95, 96, 97},
+    {98, 99, 100},
+    {101, 102, 103},
+    {104, 105, 106},
+    {107, 108, 109},
+    {110, 111, 112}
+    },
+  };
+
+  // vector<double> keys = {96, 0, 101, 100};
+
+  vector<bool> feverKeys = {false, false, true, true};
+  vector<bool> alienKeys = {false, true, false, false};
+
+  try {
+    for (int i = 0; i < 4; i++){
+      bool feverFlag = false;
+      bool alienFlag = false;
+      isAlien(tests[i], feverFlag, alienFlag);
+      if(feverFlag == feverKeys[i] && alienFlag == alienKeys[i]){
+        points.first+=1;
+      }
+    }
+  } catch(...) {
+    points.first = 0;
+  }
+  points.
+  cout << "Testing Temperature Averaging: " << points.first << "/" << points.second << endl;
+  return points;
 }
